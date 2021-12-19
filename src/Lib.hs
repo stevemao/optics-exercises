15c036e5277de79dd91944649e0997c13520fcd3
Steve Mao (beem it) <maochenyan@gmail.com>
2021-12-19 16:15:58 +1100

    exercise

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,29 @@ module Lib
     ( someFunc
     ) where
 
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+import Control.Lens
+import Control.Applicative 
+import Data.Char
+import qualified Data.Map as M 
+import qualified Data.Set as S 
+import qualified Data.Text as T
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data Ship = Ship { _numCrew :: Int
+                 , _name :: String }
+
+setName :: Ship -> String -> Ship 
+setName ship name =
+    ship{ _name = name }
+
+name :: Lens' Ship String
+name = lens _name setName
